f30339ec8882f17f88074da93859bec8e4ae930e
hkobir <hkobir10@gmail.com>
2019-11-24 00:36:36 +0600

    add title for document

---
+++ app/src/main/java/com/hk/pdfgenerate/Common/Common.java
@@ -0,0 +1,23 @@
+package com.hk.pdfgenerate.Common;
+
+import android.content.Context;
+
+import com.hk.pdfgenerate.R;
+
+import java.io.File;
+
+public class Common {
+    public static String getAppPath(Context context){
+        File dir = new File(android.os.Environment.getExternalStorageDirectory()
+                +File.separator
+                +context.getResources().getString(R.string.app_name)
+                +File.separator
+        );
+
+        if(!dir.exists()) {
+            dir.mkdir();
+        }
+            return dir.getPath()+File.separator;
+
+    }
+}
--- app/src/main/java/com/hk/pdfgenerate/MainActivity.java
+++ app/src/main/java/com/hk/pdfgenerate/MainActivity.java
@@ -2,13 +2,120 @@ package com.hk.pdfgenerate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.hk.pdfgenerate.Common.Common;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.annotation.Documented;
 
 public class MainActivity extends AppCompatActivity {
+    private Button generateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        generateBtn = findViewById(R.id.generateButton);
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+
+                        generateBtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                                createPDF(Common.getAppPath(MainActivity.this) + "1234_invoice.pdf");
+
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
     }
+
+    private void createPDF(String path) {
+        if (new File(path).exists())
+            new File(path).delete();
+        try {
+            Document document = new Document();
+            //save
+            PdfWriter.getInstance(document, new FileOutputStream(path));
+
+            //open
+            document.open();
+
+
+            // Document Settings
+            document.setPageSize(PageSize.A4);
+            document.addCreationDate();
+            document.addAuthor("KShope");
+            document.addCreator("Kshop Stuff");
+
+            //fonts setting
+            BaseColor mColorAccent = new BaseColor(0, 153, 204, 255);
+            float mHeadingFontSize = 20.0f;
+            float mValueFontSize = 26.0f;
+
+            BaseFont urName = BaseFont.createFont("assets/fonts/brandon_medium.otf", "UTF-8", BaseFont.EMBEDDED);
+
+            // Adding Title....
+            Font titleFont = new Font(urName, 35.0f, Font.NORMAL, BaseColor.BLACK);
+            addnewItem(document, "Order Details", Element.ALIGN_CENTER, titleFont);
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void addnewItem(Document document, String text, int align, Font font) throws DocumentException {
+        Chunk chunk = new Chunk(text,font);
+        Paragraph paragraph = new Paragraph(chunk);
+        paragraph.setAlignment(align);
+        document.add(paragraph);
+
+    }
+
+
 }
